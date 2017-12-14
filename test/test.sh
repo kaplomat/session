ecd55b50d5b803d5fd8ed81c90b669afe99a86ee
Mateusz Kapłon <mateusz.kaplon@tieto.com>
2017-12-14 09:57:04 +0100

    WIP test.sh script for testing the program

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-[ -f ../session ] || {
+PROG='../session'
+[ -f "$PROG" ] || {
   echo 'No `session` binary found in project directory.'
   echo 'Please `make all` before running tests!'
+  exit 1
+}
+
+echo "> $PROG --id test-sess --set name --value haha"
+$PROG --id test-sess --set name --value haha
+
+echo "$PROG --id test-sess --get name"
+val=$(> $PROG --id test-sess --get name)
+echo "$val"
+
+[ "$val" = "haha" ] || {
+  echo 'FAILURE:'
+  echo "Expected '$val' == 'haha'"
   exit 1
 }
